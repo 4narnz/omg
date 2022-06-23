cd6e79fb22ee1f2beff34581001551c6486ab54b
4narnz <smaasland@gmail.com>
2022-06-23 01:07:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -115,8 +115,7 @@ function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature = (14 * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = fahrenheitTemperature;
-  alert("link clicked");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 let form = document.querySelector("#search-form");
