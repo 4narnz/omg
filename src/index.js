0b75121dbdc66b3070f299aa20d863f2a6fd577f
4narnz <smaasland@gmail.com>
2022-06-23 01:22:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -121,7 +121,7 @@ function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-
+}
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
