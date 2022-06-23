660aaf7b430aadf997cff42f5b2bcc480489fe00
4narnz <smaasland@gmail.com>
2022-06-22 23:46:31 -0700

    update apikey

--- src/index.js
+++ src/index.js
@@ -100,8 +100,8 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "f16c4883dc2b233e5edf856878391cd1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${f16c4883dc2b233e5edf856878391cd1}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
